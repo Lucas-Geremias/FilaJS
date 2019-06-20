89acd824ec8e9ddeb8ec6b631bd9e3c40d7578c7
Lucas Santos <geremiaslucas2012@gmail.com>
2019-06-20 19:55:51 -0300

    fila

--- js/dynamic_queue.js
+++ js/dynamic_queue.js
@@ -1,4 +1,7 @@
 class DynamicQueue{
+    //O método queue () mostra a fila de funções a serem executadas nos elementos selecionados.
+//Uma fila é uma ou mais funções esperando para ser executada.
+//O método queue () pode ser usado junto com o método dequeue () .
     constructor(){
     }
     enqueue(element) {
--- js/static_queue.js
+++ js/static_queue.js
@@ -3,6 +3,9 @@ class StaticQueue{
     }
     enqueue(element) {
     }
+    //Quando .dequeue()é chamado, a próxima função na fila é removida da fila e, em seguida, 
+    //executada. Esta função deve, por sua vez (direta ou indiretamente), .dequeue()ser chamada,
+    //para que a sequência possa continuar.
     dequeue(){
     }
     front() {
