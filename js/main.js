239282741078a2d95b502c7fe10808b0d868e12f
Lucas Santos <geremiaslucas2012@gmail.com>
2019-06-20 19:53:32 -0300

    fila

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+//Declarações de variáveis, onde quer que elas ocorram, são processadas antes que qualquer 
+//outro código seja executado. O escopo de uma variável declarada com  var é seu contexto atual 
+//em execução, o qual é a função a qual pertence ou, para variáveis declaradas fora de qualquer 
+//função, o escopo é o global.
 var queue = new StaticQueue();
 $().ready(function() {
 
@@ -9,7 +13,9 @@ $().ready(function() {
     $('#dequeue').on('click', dequeueElement);
     $('#clear').on('click', clearQueue);
 });
-
+//Uma função criada com uma declaração function é um objeto Function e tem todos as propriedades, 
+//métodos e comportamentos dos objetos Function. Veja Function para detalhadas informações sobre as 
+//funções.
 function enqueueElement() {
     var item = $('#item').val();
     if (item) {
@@ -18,7 +24,7 @@ function enqueueElement() {
         showQueue();
     }
 }
-
+//Executa a próxima função na fila para o elemento correspondente.
 function dequeueElement() {
     if (queue.isEmpty() == false) {
         alert('Foi removido o elemento ' + queue.dequeue());
@@ -27,12 +33,16 @@ function dequeueElement() {
         alert('Fila Vazia');
     }
 }
-
+//Quando o .clearQueue()método é chamado, todas as funções na fila que não foram executadas são 
+//removidas da fila. Quando usado sem um argumento, .clearQueue()remove as funções restantes da 
+//fxfila de efeitos padrão. Desta forma, é semelhante a .stop(true). No entanto, embora o .stop()método
+// deva ser usado apenas com animações, .clearQueue()também pode ser usado para remover qualquer função 
+//que tenha sido adicionada a uma fila genérica do jQuery com o .queue()método.
 function clearQueue() {
     queue.clear();
     showQueue();
 }
-
+//Mostrar ou manipular a fila de funções a serem executadas nos elementos correspondentes.
 function showQueue() {
     $('#output').empty();
     $('#output').append('<div class="ui label">' +
